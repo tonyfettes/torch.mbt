40d29987baeb7459bde50f2b2f8ef6cd2083bfa7
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2024-10-20 21:05:50 +0800

    fix: set seed for transpilation snapshot tests

--- src/__snapshot__/ToPyTorchSource/Linear.py
+++ src/__snapshot__/ToPyTorchSource/Linear.py
@@ -16,12 +16,12 @@ _layer6 = torch.nn.Sequential(
 )
 
 with torch.no_grad():
-    _layer0.weight = torch.nn.Parameter(torch.tensor([[0.2319395440585279,-0.9572423457468497]]).transpose(-2, -1))
-    _layer0.bias = torch.nn.Parameter(torch.tensor([-0.6499392928503491,-0.9692349476337747]))
-    _layer2.weight = torch.nn.Parameter(torch.tensor([[-0.23773777815575792,-0.306780799224424],[-0.5823891499718423,0.48022393759696014]]).transpose(-2, -1))
-    _layer2.bias = torch.nn.Parameter(torch.tensor([0.43009576963007823,0.2353595493072772]))
-    _layer4.weight = torch.nn.Parameter(torch.tensor([[0.036133062528545534],[0.5297191538773458]]).transpose(-2, -1))
-    _layer4.bias = torch.nn.Parameter(torch.tensor([-0.47865211700599963]))
+    _layer0.weight = torch.nn.Parameter(torch.tensor([[-0.29557028390369,-0.8177422144361883]]).transpose(-2, -1))
+    _layer0.bias = torch.nn.Parameter(torch.tensor([-0.8997844018869048,-0.1545487762608706]))
+    _layer2.weight = torch.nn.Parameter(torch.tensor([[-0.39032679720623276,-0.06705152597716957],[0.46733088627300723,0.1554945481526122]]).transpose(-2, -1))
+    _layer2.bias = torch.nn.Parameter(torch.tensor([0.3050002595219734,0.6775787971268634]))
+    _layer4.weight = torch.nn.Parameter(torch.tensor([[0.5111579847372985],[-0.3668642715992912]]).transpose(-2, -1))
+    _layer4.bias = torch.nn.Parameter(torch.tensor([0.1910001136989974]))
 
 model = _layer6
 
