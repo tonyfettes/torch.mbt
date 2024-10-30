aa7c2bad67a9b4bf1f4bfebfdbfe34d22dfa6d47
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2024-10-30 09:13:21 +0800

    feat: add more control to trainer

--- test/mnist/web/mnist-trainer.ts
+++ test/mnist/web/mnist-trainer.ts
@@ -7,17 +7,29 @@ import mnistDatabase from "./mnist-database";
 class MnistTrainer extends WebComponent(HTMLElement) {
   private _logs: { step: number; loss: number }[];
   private _lossPlot: (SVGSVGElement | HTMLElement) | null;
-  // private _state: "idle" | "busy";
   private _button: HTMLButtonElement | null;
-  private _batchSize: number;
-  static override observedAttributes = ["batch-size", "learning-rate"];
+  private _batchSizeInput: HTMLInputElement | null;
+  private _learningRateInput: HTMLInputElement | null;
+  private _epochInput: HTMLInputElement | null;
+  private _training: {
+    state: "running" | "stopped";
+    epoch: number;
+    index: number;
+  };
+  static override observedAttributes = ["batch-size", "learning-rate", "epoch"];
   constructor() {
     super();
     this._logs = [];
     this._lossPlot = null;
-    // this._state = "idle";
     this._button = null;
-    this._batchSize = 1;
+    this._batchSizeInput = null;
+    this._learningRateInput = null;
+    this._epochInput = null;
+    this._training = {
+      state: "stopped",
+      epoch: 0,
+      index: 0,
+    };
   }
   private async plotLoss() {
     const newPlot = Plot.plot({
@@ -37,15 +49,30 @@ class MnistTrainer extends WebComponent(HTMLElement) {
     }
     this._lossPlot = newPlot;
   }
+  private get _epoch(): number {
+    return Number(this._epochInput?.value) ?? 1;
+  }
+  private get _batchSize(): number {
+    return Number(this._batchSizeInput?.value) ?? 1;
+  }
+  private get _learningRate(): number {
+    return Number(this._learningRateInput?.value) ?? 0.1;
+  }
   /**
    * Train the model.
    */
   async train(): Promise<void> {
-    for (let epoch = 0; epoch < 1; epoch++) {
-      for (let i = 0; i < 60000; i += this._batchSize) {
+    this._training.state = "running";
+    for (let epoch = this._training.epoch; epoch < this._epoch; epoch++) {
+      for (let i = this._training.index; i < 60000; i += this._batchSize) {
+        if (this._training.state === "stopped") {
+          this._training.epoch = epoch;
+          this._training.index = i;
+          return;
+        }
         const batch: [Float64Array, number][] = [];
-        for (let b = 0; b < this._batchSize; b++) {
-          const data = await mnistDatabase.getData("train", i + b);
+        for (let b = i; b < Math.min(i + this._batchSize, 60000); b++) {
+          const data = await mnistDatabase.getData("train", b);
           const input = new Float64Array(data.image.map((x) => x / 255));
           batch.push([input, data.label]);
         }
@@ -53,7 +80,7 @@ class MnistTrainer extends WebComponent(HTMLElement) {
           type: "train",
           data: {
             batch,
-            learningRate: 0.001,
+            learningRate: this._learningRate,
           },
         });
         await new Promise<void>((resolve) => {
@@ -78,31 +105,53 @@ class MnistTrainer extends WebComponent(HTMLElement) {
         });
       }
     }
-    if (this._button) {
-      this._button.disabled = false;
-    }
   }
   override connectedCallback() {
     this.attachShadow({ mode: "open" });
-    this.shadowRoot?.appendChild(template.content.cloneNode(true));
+    if (!this.shadowRoot) {
+      throw new Error("Failed to attach shadow");
+    }
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.plotLoss();
-    const button: HTMLButtonElement | null | undefined =
-      this.shadowRoot?.querySelector("button#start");
-    if (button) {
-      button.addEventListener("click", async () => {
-        button.disabled = true;
-        // this._state = "busy";
-        await this.train();
+    this._button = this.shadowRoot.querySelector("button#start");
+    const onClick = async () => {
+      if (!this._button) {
+        return;
+      }
+      console.log('Change the button value to "Abort"');
+      this._button.innerText = "Stop";
+      this._button?.addEventListener(
+        "click",
+        async () => {
+          this._training.state = "stopped";
+          if (this._button) {
+            this._button.innerText = "Waiting";
+          }
+        },
+        {
+          once: true,
+        }
+      );
+      this._batchSizeInput?.setAttribute("disabled", "");
+      this._learningRateInput?.setAttribute("disabled", "");
+      this._epochInput?.setAttribute("disabled", "");
+      await this.train();
+      this._button.innerText = "Start";
+      this._button.addEventListener("click", onClick, {
+        once: true,
       });
-      this._button = button;
-    }
-    const batchSizeInput: HTMLInputElement | null | undefined =
-      this.shadowRoot?.querySelector("input#batch-size");
-    if (batchSizeInput) {
-      batchSizeInput.addEventListener("input", () => {
-        this._batchSize = Number(batchSizeInput.value);
-      });
-    }
+      this._batchSizeInput?.removeAttribute("disabled");
+      this._learningRateInput?.removeAttribute("disabled");
+      this._epochInput?.removeAttribute("disabled");
+    };
+    this._button?.addEventListener("click", onClick, {
+      once: true,
+    });
+    this._batchSizeInput = this.shadowRoot?.querySelector("input#batch-size");
+    this._learningRateInput = this.shadowRoot?.querySelector(
+      "input#learning-rate"
+    );
+    this._epochInput = this.shadowRoot?.querySelector("input#epoch");
   }
 }
 
